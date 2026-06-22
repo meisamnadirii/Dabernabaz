8b275bac5248cc225964fd0a95291039c5c96437
meisamnadirii <meisamnadirii@gmail.com>
2026-06-22 18:05:04 +0330

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let balance = 2750;
-function updateBalance() { document.getElementById('balance').innerText = balance.toLocaleString(); }
-function startGame() {
-    if (balance >= 500) { balance -= 500; updateBalance(); alert("بازی شروع شد! ۵۰۰ تومان کسر شد."); }
-    else { alert("موجودی کافی نیست!"); }
-}
